0015b2a9f18fefd44763acfe74ad64c9dff53bf6
mlx <778683342@qq.com>
2023-09-16 00:52:20 +0800

    add book

--- chapter-one/bill-calc-render-separate.js
+++ chapter-one/bill-calc-render-separate.js
@@ -1,8 +1,8 @@
-function totalAmount(data) {
-    return data.performances
-        .reduce((total, p) => total + p.amount, 0);
-}
-function totalVolumeCredits(data) {
-    return data.performances
-        .reduce((total, p) => total + p.volumeCredits, 0);
+function createPerformanceCalculator(aPerformance, aPlay) {
+    switch (aPlay.type) {
+        case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error(`unknown type: ${aPlay.type}`);
+    }
 }
